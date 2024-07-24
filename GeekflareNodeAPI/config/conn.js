99ae24c2ba52e9a39d182b19536ba6c7b5a5266d
javier <javisrm1@gmail.com>
2024-07-24 13:07:47 -0500

    servidor echo en nodjs y angular

---
+++ GeekflareNodeAPI/config/conn.js
@@ -0,0 +1,10 @@
+/* const mongoose = require('mongoose');
+
+mongoose.connect(process.env.URI, 
+    { useNewUrlParser: true,
+     useUnifiedTopology: true })
+    .then((data) => {
+        console.log(`Database connected to ${data.connection.host}`)
+})
+
+ */
